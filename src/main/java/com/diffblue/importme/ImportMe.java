be29b2a143c79d6a15ba237da058f9d46f9249de
Freddy Tuxworth <freddytuxworth@gmail.com>
2019-03-04 11:39:26 +0000

    y

---
+++ src/main/java/com/diffblue/importme/ImportMe.java
@@ -0,0 +1,7 @@
+package com.diffblue.importme;
+
+public class ImportMe {
+	public static boolean hasGot(){
+		return true;
+	}
+}
--- src/main/java/com/diffblue/javademo/IncludeTest.java
+++ src/main/java/com/diffblue/javademo/IncludeTest.java
@@ -1,6 +1,12 @@
 package com.diffblue.javademo;
 
+import com.diffblue.importme.ImportMe;
+
 public class IncludeTest {
+
+        public boolean hasGot(){
+		return com.diffblue.importme.ImportMe.hasGot();
+	}
 
 	public boolean needsFoo(String input){
 		if(input.equals("youll_never_guess_me")){
