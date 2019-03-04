0c47ba51f26ee0e0e45af236502adb42cc438508
Freddy Tuxworth <freddytuxworth@gmail.com>
2019-03-04 11:47:22 +0000

    ao

--- src/main/java/com/diffblue/javademo/IncludeTest.java
+++ src/main/java/com/diffblue/javademo/IncludeTest.java
@@ -8,19 +8,4 @@ public class IncludeTest {
 		return com.diffblue.importme.ImportMe.hasGot();
 	}
 
-	public boolean needsFoo(String input){
-		if(input.equals("youll_never_guess_me")){
-			return true;
-		}
-		return false;
-	}
-
-	public boolean needsBar(String input){
-		if(input.equals("youll_never_guess_me")){
-			return true;
-		} else {
-			return false;
-		}
-	}
-
 }
