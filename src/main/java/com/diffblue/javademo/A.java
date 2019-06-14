4284ccaf6f4d37b3389e75dda972ef36c2f836e6
Freddy Tuxworth <freddytuxworth@gmail.com>
2019-06-14 16:09:38 +0100

    removes

---
+++ src/main/java/com/diffblue/javademo/A.java
@@ -0,0 +1,14 @@
+package com.diffblue.javademo;
+
+import java.util.ArrayList;
+
+public class A {
+	public A[][] r(){
+		A[][] a = new A[][]{
+			{new A(), new A(), new A()},
+			{new A(), new A(), new A()},
+			{new A(), new A(), new A()}
+		};
+		return a;
+	}
+}
